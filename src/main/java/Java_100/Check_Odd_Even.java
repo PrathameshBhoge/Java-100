b5d53bd944da0263482cb29d128474079e965724
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-06-16 10:03:41 +0530

    initial commit

---
+++ src/main/java/Java_100/Check_Odd_Even.java
@@ -0,0 +1,12 @@
+package Java_100;
+
+public class Check_Odd_Even {
+    public static void main(String[] args) {
+        int x = 29;
+        if(x % 2 ==0){
+            System.out.println("The Number "+x+"is even ");
+        }else{
+            System.out.println("The number "+x+"is odd");
+        }
+    }
+}
