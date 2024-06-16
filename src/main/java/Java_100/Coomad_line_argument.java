943ccab089ee957e5f9d37abee98516e99bef645
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-06-16 10:03:57 +0530

    initial commit

---
+++ src/main/java/Java_100/Coomad_line_argument.java
@@ -0,0 +1,9 @@
+package Java_100;
+
+public class Coomad_line_argument {
+    public static void main(String[] args) {
+        for(String i : args){
+            System.out.println(i);
+        }
+    }
+}
