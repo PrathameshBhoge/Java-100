2dacdad46941502fecf99b213dce3e05d27a3386
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-06-19 19:42:49 +0530

    initial commit

---
+++ src/main/java/Java_100/If_else_ex1.java
@@ -0,0 +1,12 @@
+package Java_100;
+
+public class If_else_ex1 {
+    public static void main(String[] args) {
+        boolean learning = true;
+        if(learning){
+            System.out.println("Java Learner");
+        }else{
+            System.out.println("Python Learner");
+        }
+    }
+}
